298ee5a5443cde394b9807de6e8e4ebaf12c6d6d
yugsho <emiliorenders@Emilios-MacBook-Pro.local>
2020-10-25 23:26:16 +0100

    workpage-styling

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import {
 import Home from "./home";
 import About from "./about";
 import Work from "./work";
-import Contact from "./contact";
 import {Box} from '@material-ui/core';
 
 //This React class acts as the navigation of the application
@@ -21,13 +20,11 @@ class Main extends Component {
             <li><NavLink exact to="/">Home</NavLink></li>
             <li><NavLink to="/about">About</NavLink></li>
             <li><NavLink to="/work">Work</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <Box className="content">
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/work" component={Work}/>
-            <Route path="/contact" component={Contact}/>
           </Box>
           </HashRouter>
       </Box>
